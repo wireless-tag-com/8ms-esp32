7b9394a559efa2327b5bc7cc1211d6bd59d14bf6
BigBee <295677896@qq.com>
2021-11-10 09:10:08 +0800

    modified *.h

--- components/blockly/lv_8ms_http.h
+++ components/blockly/lv_8ms_http.h
@@ -1,6 +1,19 @@
 #ifndef __LV_8MS_HTTP_H
 #define __LV_8MS_HTTP_H
 typedef int (*_8ms_http_cb_t)(char* data);
+/**
+ * @brief 发送Http请求
+ * 
+ * @param url  请求接口（网址）
+ * @param port 要连接的端口
+ * @param path 访问资源的路径
+ */
 void lv_8ms_httprequest(const char* url,const char* port,const char* path);
+
+/**
+ * @brief 设置http回调函数
+ * 
+ * @param cb 回调函数指针
+ */
 void lv_8ms_set_http_cb(_8ms_http_cb_t cb);
 #endif
